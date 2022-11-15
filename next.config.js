ec883fee2242eae26298cc8932ec591d514f4eae
milshop <45132138+milshop@users.noreply.github.com>
2022-11-15 21:38:04 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  images: {
-    loader: 'static'
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+
+const images = {
+  loader: "imgix",
+  path: "https://noop/",
+}
+module.exports = nextConfig,images
